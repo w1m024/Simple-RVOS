51aa8ce90880dfca613c59e8041b082fe875c838
w1m024 <2213794417@qq.com>
2025-04-22 19:39:48 +0800

    完成上下文切换功能

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,15 +4,20 @@ extern void uart_init(void);
 extern void uart_puts(char* str);
 extern void page_init(void);
 extern void page_test(void);
+extern void sched_init(void);
+extern void schedule(void);
 
 void start_kernel(void) {
   uart_init();
   uart_puts("Hello, world!\n");
 
   page_init();
+  // page_test();
+  // uart_puts("page test end.\n");
 
-  page_test();
+  sched_init();
+  schedule();
+  uart_puts("sched test end.\n");
 
-  uart_puts("page test end.\n");
   while (1) {};
 }
